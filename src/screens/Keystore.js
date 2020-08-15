da57b728f3d502b9e6821707c53a2188c5914531
Ananth Sekar <ananth.sekar@gogsoon.com>
2020-08-16 01:18:40 +0530

    [Ananth] Adds FAB icon to keystore

--- src/screens/Keystore.js
+++ src/screens/Keystore.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import AppLayout from "./../AppLayout/AppLayout"
+import AppLayout from "./../AppLayout/AppLayout";
+import CustomFab from "./../components/Customfab/CustomFab"
 export default class Keystore extends Component {
   constructor(props) {
     super(props);
@@ -12,6 +13,7 @@ export default class Keystore extends Component {
     return (
       <View>
       <AppLayout navigation={this.props.navigation} title="Key Store" />
+      <CustomFab   iconName={'plus'} />
       </View>
     );
   }
