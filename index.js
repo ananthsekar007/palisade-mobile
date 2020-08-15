413dd69e6255f4b61ecc2d584b48713dd4670890
Ananth Sekar <ananth.sekar@gogsoon.com>
2020-08-16 02:09:59 +0530

    [Ananth] Adds snackbar

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 /**
  * @format
  */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {Provider as PaperProvider, DefaultTheme} from 'react-native-paper';
+import {SnackProvider} from 'react-native-snackbar-reddit';
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    accent: 'yellow',
+  },
+};
 
-AppRegistry.registerComponent(appName, () => App);
+const FullApp = () => {
+  return (
+    <PaperProvider theme={theme}>
+      <SnackProvider>
+        <App />
+      </SnackProvider>
+    </PaperProvider>
+  );
+};
+
+AppRegistry.registerComponent(appName, () => FullApp);
