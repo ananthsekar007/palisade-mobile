d3335b9902bcbe5cb06619b4dddcbc43ea5724e2
Ananth Sekar <ananth.sekar@gogsoon.com>
2020-08-16 00:39:29 +0530

    [Ananth] Adds App  layout for archieved tasks

--- src/screens/Tasks/ArchievedTasks.js
+++ src/screens/Tasks/ArchievedTasks.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-
+import Icon from 'react-native-vector-icons/FontAwesome';
+import AppLayout from './../../AppLayout/AppLayout';
 export default class ArchievedTasks extends Component {
   constructor(props) {
     super(props);
@@ -11,7 +12,7 @@ export default class ArchievedTasks extends Component {
   render() {
     return (
       <View>
-        <Text> ArchievedTasks </Text>
+      <AppLayout navigation={this.props.navigation} title="Archieved Tasks" />
       </View>
     );
   }
