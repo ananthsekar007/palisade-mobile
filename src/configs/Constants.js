0c335494fbb1f8539eda3ad2f1c836e83d62e807
Ananth Sekar <ananth.sekar@gogsoon.com>
2020-08-16 19:45:10 +0530

    [Ananth] Modifies base url

--- src/configs/Constants.js
+++ src/configs/Constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://192.168.0.104/palisade-backend/public/api/';
+export const BASE_URL = 'http://192.168.43.125/palisade-backend/public/api/';
 export const AUTH_TOKEN = "AUTH_TOKEN"
 export const USER_INFO = "USER_INFO"
