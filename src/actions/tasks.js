8fca49c1658fb5ff864b722a4160d537ad678460
Ananth Sekar <ananth.sekar@gogsoon.com>
2020-08-16 19:46:33 +0530

    [Ananth] Adds get all tasks HTTP request

---
+++ src/actions/tasks.js
@@ -0,0 +1,12 @@
+import {BASE_URL} from './../configs/Constants';
+import {getAuthToken} from "./auth";
+import {post, get, Delete, put} from './fetch_requests';
+
+export const getAllTasks = async () => {
+    let token = await getAuthToken();
+    const header = new Headers();
+    header.append('Content-Type', 'application/json');
+    header.append('Authorization', `Bearer ${token}`);
+
+    return get(`${BASE_URL}tasks`, header);
+}
