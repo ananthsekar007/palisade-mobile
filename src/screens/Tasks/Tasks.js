b8becc7f7c013038c2f1e1d257dd90f7e6391923
Ananth Sekar <ananth.sekar@gogsoon.com>
2020-08-16 18:34:42 +0530

    [Ananth] Implements modal component

--- src/screens/Tasks/Tasks.js
+++ src/screens/Tasks/Tasks.js
@@ -1,19 +1,37 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { Modal, Portal, Button } from 'react-native-paper';
+import CustomModal from "./../../components/CustomModal/CustomModal";
 import AppLayout from './../../AppLayout/AppLayout';
 import CustomFab from './../../components/Customfab/CustomFab';
 export default class Tasks extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+        visible: false
+    };
+    this.hideModal = this.hideModal.bind(this);
+    this.showModal = this.showModal.bind(this);
   }
 
+  hideModal = () => {
+    this.setState({
+        visible: false
+    })
+  }
+
+  showModal = () => {
+    this.setState({
+        visible: true
+    })
+  }
 
   render() {
     return (
         <AppLayout navigation={this.props.navigation} title="Tasks">
-          <CustomFab iconName={'plus'} />
+          <CustomModal visible={this.state.visible} header="Add Task" hideModal={this.hideModal}>
+          </CustomModal>
+          <CustomFab iconName={'plus'} onPress={this.showModal} />
         </AppLayout>
     );
   }
