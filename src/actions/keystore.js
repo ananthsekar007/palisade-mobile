5941408474e871103ad95006b7f01dcb72d9eb9d
Ananth Sekar <ananth.sekar@gogsoon.com>
2020-08-16 23:56:38 +0530

    [Ananth] Adds HTTP request to add keys

--- src/actions/keystore.js
+++ src/actions/keystore.js
@@ -10,3 +10,12 @@ export const getAllKeys = async () => {
 
     return get(`${BASE_URL}keystore`, header);
 }
+
+export const addKeys = async (body) => {
+    let token = await getAuthToken();
+    const header = new Headers();
+    header.append('Content-Type', 'application/json');
+    header.append('Authorization', `Bearer ${token}`);
+
+    return post(`${BASE_URL}keystore`, header, body);
+}
