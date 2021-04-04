384cea02a0dd9a117621e200ab7741b9d0d0d371
ananthsekar007 <55542809+ananthsekar007@users.noreply.github.com>
2021-04-04 11:18:54 +0530

    Changed base url

--- src/configs/Constants.js
+++ src/configs/Constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://dev.gogosoon.com/ananth/palisade-backend/public/api/';
+export const BASE_URL = 'http://localhost:8000/ananth/palisade-backend/public/api/';
 export const AUTH_TOKEN = "AUTH_TOKEN"
 export const USER_INFO = "USER_INFO"
 export const DECRYPT_KEY = "DECRYPT_KEY"
